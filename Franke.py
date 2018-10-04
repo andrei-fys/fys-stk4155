25eabaf89f5e9a7c924d52496997e3cc0d285559
Andrei-laptop <andreiku@fys.uio.no>
2018-10-04 21:32:47 +0200

    add func

--- Franke.py
+++ Franke.py
@@ -16,52 +16,62 @@ def FrankeFunction(x,y):
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
     return term1 + term2 + term3 + term4
 
-
+def SetUpDesignMat(x,y,N):
+    "Set up matrix for the polynomial approximation	"
+    assert len(x)==len(y)
+    # Degree 1
+    degree_one = x + y
+    # Degree 2
+    degree_two = x**2 + 2*x*y + y**2
+    #Degree 3
+    degree_three = x**3 + 3*x**2*y + 3*y**2*x + y**3
+    # Degree 4
+    degree_four = x**4 + 4*x**3*y + 6*x**2*y**2 + 4*y**3*x + y**4
+    # Degree 5
+    degree_five = x**5 + 5*x**4*y + 10*x**3*y**2 + 10*x**2*y**3 + 5*y**4*x + y**5
+    return  np.hstack([np.ones((N*N,1)), degree_one, degree_two, degree_three, degree_four, degree_three])
+	
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 # Make data.
 x_exact = np.arange(0, 1, 0.05)
 y_exact = np.arange(0, 1, 0.05)
-
-x = np.sort(np.random.uniform(0,1,20))
-y = np.sort(np.random.uniform(0,1,20))
-x = x_exact
-y = y_exact
+N = 20
+x = np.sort(np.random.uniform(0,1,N))
+y = np.sort(np.random.uniform(0,1,N))
 #mu, sigma = 0.5, 0.28
-#x += np.random.normal(mu, sigma, 20)
-#y += np.random.normal(mu, sigma, 20)
+#x += np.random.normal(mu, sigma, N)
+#y += np.random.normal(mu, sigma, N)
 x = np.sort(x)
 y = np.sort(y)
 x, y = np.meshgrid(x,y)
 z = FrankeFunction(x,y)
-
-
 #print(np.shape(y))
 
 #data = np.c_[np.ones((20,1)), x, y, x**2, x*y, y**2, x**3, x**2*y, x*y**2, y**3, x**4, x**3*y, x**2*y**2, x*y**3,y**4, x**5, x**4*y, x**3*y**2, x**2*y**3,x*y**4, y**5]
 
 x = x.reshape(-1, 1)
 y = y.reshape(-1, 1)
-z = z.reshape(400,1)
+z = z.reshape(N*N,1)
 
 #print(np.shape(x))
-data = np.hstack([np.ones((400,1)), x + y, x**2 + 2*x*y + y**2, x**3 + 3*x**2*y + 3*x*y**2 + y**3, x**4 + 4*x**3*y + 6*x**2*y**2 + 4*x*y**3 + y**4, x**5 + 5*x**4*y + 10*x**3*y**2 + 10*x**2*y**3 + 5*x*y**4 + y**5])
+#data = np.hstack([np.ones((400,1)), x + y, x**2 + 2*x*y + y**2, x**3 + 3*x**2*y + 3*x*y**2 + y**3, x**4 + 4*x**3*y + 6*x**2*y**2 + 4*x*y**3 + y**4, x**5 + 5*x**4*y + 10*x**3*y**2 + 10*x**2*y**3 + 5*x*y**4 + y**5])
+data = SetUpDesignMat(x,y,N)
 clf5 = LinearRegression()
 clf5.fit(data,z)
 
-x_n = np.sort(np.random.uniform(0,1,20))
-y_n = np.sort(np.random.uniform(0,1,20))
-x_n = x_exact
-y_n = y_exact
+x_n = np.sort(np.random.uniform(0,1,N))
+y_n = np.sort(np.random.uniform(0,1,N))
 #mu, sigma = 0.5, 0.28
-#x += np.random.normal(mu, sigma, 20)
-#y += np.random.normal(mu, sigma, 20)
+#x += np.random.normal(mu, sigma, N)
+#y += np.random.normal(mu, sigma, N)
 x_n = np.sort(x_n)
 y_n = np.sort(y_n)
 x_n, y_n = np.meshgrid(x_n,y_n)
 x_n = x_n.reshape(-1, 1)
 y_n = y_n.reshape(-1, 1)
-data_n = np.hstack([np.ones((400,1)), x_n + y_n, x_n**2 + 2*x_n*y_n + y_n**2, x_n**3 + 3*x_n**2*y_n + 3*x_n*y_n**2 + y_n**3, x_n**4 + 4*x_n**3*y_n + 6*x_n**2*y_n**2 + 4*x_n*y_n**3 + y_n**4, x_n**5 + 5*x_n**4*y_n + 10*x_n**3*y_n**2 + 10*x_n**2*y_n**3 + 5*x_n*y_n**4 + y_n**5])
+#data_n = np.hstack([np.ones((400,1)), x_n + y_n, x_n**2 + 2*x_n*y_n + y_n**2, x_n**3 + 3*x_n**2*y_n + 3*x_n*y_n**2 + y_n**3, x_n**4 + 4*x_n**3*y_n + 6*x_n**2*y_n**2 + 4*x_n*y_n**3 + y_n**4, x_n**5 + 5*x_n**4*y_n + 10*x_n**3*y_n**2 + 10*x_n**2*y_n**3 + 5*x_n*y_n**4 + y_n**5])
+data_n = SetUpDesignMat(x_n,y_n,N)
 z_n = clf5.predict(data_n)
 R2 = clf5.score(data_n, z.reshape(-1, 1))
 MSE = mean_squared_error(z.reshape(-1, 1), z_n)
