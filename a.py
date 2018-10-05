5d1b0810714d8f6a672d947f894629c2b94e9d9d
Andrei-laptop <andreiku@fys.uio.no>
2018-10-05 13:00:16 +0200

    Ridge, Lasso

--- a.py
+++ a.py
@@ -4,7 +4,7 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 from random import random, seed
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression,RidgeCV,Lasso
 from sklearn.metrics import mean_squared_error, r2_score
 from Franke import FrankeFunction, SetUpDesignMat, SetUpGrid
 
@@ -23,33 +23,17 @@ def ScikitSolverOLS(data, z):
     print('Coefficient beta : \n', clf5.coef_)
 
 def ScikitSolverRidge(data, z):
-    clf5 = LinearRegression()
-    clf5.fit(data,z)
-    x_n, y_n = SetUpGrid(N)
-    x_n = x_n.reshape(-1, 1)
-    y_n = y_n.reshape(-1, 1)
-    data_n = SetUpDesignMat(x_n,y_n,N)
-    z_n = clf5.predict(data_n)
-    R2 = clf5.score(data_n, z.reshape(-1, 1))
-    MSE = mean_squared_error(z.reshape(-1, 1), z_n)
-    print(R2)
-    print(MSE)
-    print('Coefficient beta : \n', clf5.coef_)
-
-#def ScikitSolverLasso(data, z):
-#    clf5 = LinearRegression()
-#    clf5.fit(data,z)
-#    x_n, y_n = SetUpGrid(N)
-#    x_n = x_n.reshape(-1, 1)
-#    y_n = y_n.reshape(-1, 1)
-#    data_n = SetUpDesignMat(x_n,y_n,N)
-#    z_n = clf5.predict(data_n)
-#    R2 = clf5.score(data_n, z.reshape(-1, 1))
-#    MSE = mean_squared_error(z.reshape(-1, 1), z_n)
-#    print(R2)
-#    print(MSE)
-#    print('Coefficient beta : \n', clf5.coef_)
-
+    ridge=RidgeCV(alphas=[0.1,1.0,10.0])
+    ridge.fit(data,z)
+    print("Ridge Coefficient: ",ridge.coef_)
+    print("Ridge Intercept: ", ridge.intercept_)
+    
+def ScikitSolverLasso(data, z):
+    lasso=Lasso(alpha=0.1)
+    lasso.fit(data,z)
+    predl=lasso.predict(data)
+    print("Lasso Coefficient: ", lasso.coef_)
+    print("Lasso Intercept: ", lasso.intercept_)
 
 
 def NaiveSolver(data, z):	
@@ -78,6 +62,6 @@ z = z.reshape(N*N,1)
 data = SetUpDesignMat(x,y,N)
 
 ScikitSolverOLS(data, z)
-#ScikitSolverRidge(data, z)
-#ScikitSolverLasso(data, z)
+ScikitSolverRidge(data, z)
+ScikitSolverLasso(data, z)
 NaiveSolver(data, z)
