2354352e0dc58345c69a101edf549114e6569a6b
AnnaGribkovskaya <anna.gribkovskaya@gmail.com>
2018-10-05 11:29:11 +0200

    just optimizing the code

--- Franke.py
+++ Franke.py
@@ -30,6 +30,17 @@ def SetUpDesignMat(x,y,N):
     # Degree 5
     degree_five = x**5 + 5*x**4*y + 10*x**3*y**2 + 10*x**2*y**3 + 5*y**4*x + y**5
     return  np.hstack([np.ones((N*N,1)), degree_one, degree_two, degree_three, degree_four, degree_five])
+
+def SetUpGrid(N):    
+    x = np.sort(np.random.uniform(0,1,N))
+    y = np.sort(np.random.uniform(0,1,N))
+    #mu, sigma = 0.5, 0.28
+    #x += np.random.normal(mu, sigma, N)
+    #y += np.random.normal(mu, sigma, N)
+    x = np.sort(x)
+    y = np.sort(y)
+    x, y = np.meshgrid(x,y)
+    return x, y
 	
 fig = plt.figure()
 ax = fig.gca(projection='3d')
@@ -37,14 +48,14 @@ ax = fig.gca(projection='3d')
 x_exact = np.arange(0, 1, 0.05)
 y_exact = np.arange(0, 1, 0.05)
 N = 20
-x = np.sort(np.random.uniform(0,1,N))
-y = np.sort(np.random.uniform(0,1,N))
+#x = np.sort(np.random.uniform(0,1,N))
+#y = np.sort(np.random.uniform(0,1,N))
 #mu, sigma = 0.5, 0.28
 #x += np.random.normal(mu, sigma, N)
 #y += np.random.normal(mu, sigma, N)
-x = np.sort(x)
-y = np.sort(y)
-x, y = np.meshgrid(x,y)
+#x = np.sort(x)
+#y = np.sort(y)
+x, y = SetUpGrid(N)
 z = FrankeFunction(x,y)
 #print(np.shape(y))
 
@@ -55,22 +66,20 @@ y = y.reshape(-1, 1)
 z = z.reshape(N*N,1)
 
 #print(np.shape(x))
-#data = np.hstack([np.ones((400,1)), x + y, x**2 + 2*x*y + y**2, x**3 + 3*x**2*y + 3*x*y**2 + y**3, x**4 + 4*x**3*y + 6*x**2*y**2 + 4*x*y**3 + y**4, x**5 + 5*x**4*y + 10*x**3*y**2 + 10*x**2*y**3 + 5*x*y**4 + y**5])
 data = SetUpDesignMat(x,y,N)
 clf5 = LinearRegression()
 clf5.fit(data,z)
 
-x_n = np.sort(np.random.uniform(0,1,N))
-y_n = np.sort(np.random.uniform(0,1,N))
+#x_n = np.sort(np.random.uniform(0,1,N))
+#y_n = np.sort(np.random.uniform(0,1,N))
 #mu, sigma = 0.5, 0.28
 #x += np.random.normal(mu, sigma, N)
 #y += np.random.normal(mu, sigma, N)
-x_n = np.sort(x_n)
-y_n = np.sort(y_n)
-x_n, y_n = np.meshgrid(x_n,y_n)
+#x_n = np.sort(x_n)
+#y_n = np.sort(y_n)
+x_n, y_n = SetUpGrid(N)
 x_n = x_n.reshape(-1, 1)
 y_n = y_n.reshape(-1, 1)
-#data_n = np.hstack([np.ones((400,1)), x_n + y_n, x_n**2 + 2*x_n*y_n + y_n**2, x_n**3 + 3*x_n**2*y_n + 3*x_n*y_n**2 + y_n**3, x_n**4 + 4*x_n**3*y_n + 6*x_n**2*y_n**2 + 4*x_n*y_n**3 + y_n**4, x_n**5 + 5*x_n**4*y_n + 10*x_n**3*y_n**2 + 10*x_n**2*y_n**3 + 5*x_n*y_n**4 + y_n**5])
 data_n = SetUpDesignMat(x_n,y_n,N)
 z_n = clf5.predict(data_n)
 R2 = clf5.score(data_n, z.reshape(-1, 1))
