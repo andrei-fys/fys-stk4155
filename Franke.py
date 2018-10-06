09628796876b0f6e6f13c9a38bbdaa5b16232476
Andrei-laptop <andreiku@fys.uio.no>
2018-10-06 16:14:21 +0200

    poly degree var

--- Franke.py
+++ Franke.py
@@ -16,20 +16,62 @@ def FrankeFunction(x,y):
     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
     return term1 + term2 + term3 + term4
 
-def SetUpDesignMat(x,y,N):
-    "Set up matrix for the polynomial approximation	"
+#def SetUpDesignMat(x,y,N,degree):
+#    "Set up matrix for the polynomial approximation	"
+#    assert len(x)==len(y)
+#    # Degree 1
+#    degree_one = x + y
+#    # Degree 2
+#    degree_two = x**2 + 2*x*y + y**2
+#    #Degree 3
+#    degree_three = x**3 + 3*x**2*y + 3*y**2*x + y**3
+#    # Degree 4
+#    degree_four = x**4 + 4*x**3*y + 6*x**2*y**2 + 4*y**3*x + y**4
+#    # Degree 5
+#    degree_five = x**5 + 5*x**4*y + 10*x**3*y**2 + 10*x**2*y**3 + 5*y**4*x + y**5
+#    return  np.hstack([np.ones((N*N,1)), degree_one, degree_two, degree_three, degree_four, degree_five])
+
+
+def SetUpDesignMat(x,y,N,degree):
+    switcher = {1: one(x,y,N), 2: two(x,y,N), 3: three(x,y,N), 4: four(x,y,N), 5: five(x,y,N)}
+    polynomial = switcher.get(degree, lambda: "Invalid degree")
+    return polynomial
+
+def one(x,y,N):
     assert len(x)==len(y)
-    # Degree 1
     degree_one = x + y
-    # Degree 2
+    return np.hstack([np.ones((N*N,1)), degree_one])
+
+def two(x,y,N):
+    assert len(x)==len(y)
+    degree_one = x + y
     degree_two = x**2 + 2*x*y + y**2
-    #Degree 3
+    return np.hstack([np.ones((N*N,1)), degree_one, degree_two])
+
+def three(x,y,N):
+    assert len(x)==len(y)
+    degree_one = x + y
+    degree_two = x**2 + 2*x*y + y**2
     degree_three = x**3 + 3*x**2*y + 3*y**2*x + y**3
-    # Degree 4
+    return np.hstack([np.ones((N*N,1)), degree_one, degree_two, degree_three])
+
+def four(x,y,N):
+    assert len(x)==len(y)
+    degree_one = x + y
+    degree_two = x**2 + 2*x*y + y**2
+    degree_three = x**3 + 3*x**2*y + 3*y**2*x + y**3
     degree_four = x**4 + 4*x**3*y + 6*x**2*y**2 + 4*y**3*x + y**4
-    # Degree 5
+    return  np.hstack([np.ones((N*N,1)), degree_one, degree_two, degree_three, degree_four])
+
+def five(x,y,N):
+    assert len(x)==len(y)
+    degree_one = x + y
+    degree_two = x**2 + 2*x*y + y**2
+    degree_three = x**3 + 3*x**2*y + 3*y**2*x + y**3
+    degree_four = x**4 + 4*x**3*y + 6*x**2*y**2 + 4*y**3*x + y**4
     degree_five = x**5 + 5*x**4*y + 10*x**3*y**2 + 10*x**2*y**3 + 5*y**4*x + y**5
     return  np.hstack([np.ones((N*N,1)), degree_one, degree_two, degree_three, degree_four, degree_five])
+
 
 def SetUpGrid(N):    
     x = np.sort(np.random.uniform(0,1,N))
@@ -43,19 +85,19 @@ def SetUpGrid(N):
     return x, y
 	
 
-def OSL(N):
+def OSL(N,degree):
     x, y = SetUpGrid(N)
     z = FrankeFunction(x,y)
     x = x.reshape(-1, 1)
     y = y.reshape(-1, 1)
     z = z.reshape(N*N,1)
-    data = SetUpDesignMat(x,y,N)
+    data = SetUpDesignMat(x,y,N,degree)
     LinearReg = LinearRegression()
     LinearReg.fit(data,z)
     x_n, y_n = SetUpGrid(N)
     x_n = x_n.reshape(-1, 1)
     y_n = y_n.reshape(-1, 1)
-    data_n = SetUpDesignMat(x_n,y_n,N)
+    data_n = SetUpDesignMat(x_n,y_n,N,degree)
     z_n = LinearReg.predict(data_n)
     R2 = LinearReg.score(data_n, z.reshape(-1, 1))
     MSE = mean_squared_error(z.reshape(-1, 1), z_n)
@@ -68,23 +110,23 @@ def OSL(N):
 
 
 
-#fig = plt.figure()
-#ax = fig.gca(projection='3d')
-# Make data.
-#x_exact = np.arange(0, 1, 0.05)
-#y_exact = np.arange(0, 1, 0.05)
-#N = 500
-
-#z = FrankeFunction(x_exact, y_exacti) 
-## Plot the surface.
-#surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
-#linewidth=0, antialiased=False)
-## Customize the z axis.
-#ax.set_zlim(-0.10, 1.40)
-#ax.zaxis.set_major_locator(LinearLocator(10))
-#ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-## Add a color bar which maps values to colors.
-#fig.colorbar(surf, shrink=0.5, aspect=5)
-#plt.show()
 if __name__ == '__main__':
-    OSL(20)
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    # Make data.
+    x_exact = np.arange(0, 1, 0.05)
+    y_exact = np.arange(0, 1, 0.05)
+    N = 500
+    
+    z = FrankeFunction(x_exact, y_exact) 
+    # Plot the surface.
+    surf = ax.plot_surface(x_exact, y_exact, z, cmap=cm.coolwarm,
+    linewidth=0, antialiased=False)
+    # Customize the z axis.
+    ax.set_zlim(-0.10, 1.40)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.show()
+#    OSL(20)
--- a.py
+++ a.py
@@ -1,21 +1,22 @@
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.ticker import LinearLocator, FormatStrFormatter
+#!/usr/bin/env python
+#from mpl_toolkits.mplot3d import Axes3D
+#import matplotlib.pyplot as plt
+#from matplotlib import cm
+#from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
 import numpy as np
 from random import random, seed
 from sklearn.linear_model import LinearRegression,RidgeCV,Lasso, Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 from Franke import FrankeFunction, SetUpDesignMat, SetUpGrid
 
-def ScikitSolverOLS(data, z):
-   
+def ScikitSolverOLS(data, z, degree):
     clf5 = LinearRegression(fit_intercept=False)
     clf5.fit(data,z)
     x_n, y_n = SetUpGrid(N)
     x_n = x_n.reshape(-1, 1)
     y_n = y_n.reshape(-1, 1)
-    data_n = SetUpDesignMat(x_n,y_n,N)
+    data_n = SetUpDesignMat(x_n,y_n,N,degree)
     z_n = clf5.predict(data_n)
     R2 = clf5.score(data_n, z.reshape(-1, 1))
     MSE = mean_squared_error(z.reshape(-1, 1), z_n)
@@ -24,7 +25,6 @@ def ScikitSolverOLS(data, z):
     print('Coefficient beta : \n', clf5.coef_)
 
 def ScikitSolverRidge(data, z):
-
     #ridge=RidgeCV(alphas=[0.1,1.0,10.0])
     ridge=Ridge(alpha=0.1, fit_intercept=False)
     ridge.fit(data,z)
@@ -32,7 +32,6 @@ def ScikitSolverRidge(data, z):
     print("Ridge Intercept: ", ridge.intercept_)
     
 def ScikitSolverLasso(data, z):
-   
     lasso=Lasso(alpha=0.1)
     lasso.fit(data,z)
     predl=lasso.predict(data)
@@ -40,7 +39,6 @@ def ScikitSolverLasso(data, z):
     print("Lasso Intercept: ", lasso.intercept_)
 
 def NaiveSolverOLS(z, data, data_n, x_n, y_n):
-
     beta = np.linalg.inv(data.T.dot(data)).dot(data.T).dot(z)
     zpredict = data_n.dot(beta)
     #print('Coefficient beta naive: \n', beta.reshape(1,-1))
@@ -51,9 +49,8 @@ def NaiveSolverOLS(z, data, data_n, x_n, y_n):
     #print('MSE naive  ', MSE(z, zpredict))
     return beta
 
-def NaiveSolverRidge(data, z, x_n, y_n, data_n):
-    
-    I = np.identity(6)	
+def NaiveSolverRidge(data, z, x_n, y_n, data_n, degree):
+    I = np.identity(degree+1)	
     lambda_parameter = 0.1
     beta = np.linalg.inv(data.T.dot(data) + lambda_parameter*I).dot(data.T).dot(z)
     zpredict = data_n.dot(beta)
@@ -87,54 +84,54 @@ def MSE(y, y_predict):
     s /=float(n)
     return s
 
-def SetUpData(N):
+def SetUpData(N, degree):
     x, y = SetUpGrid(N)
     z = FrankeFunction(x,y)
     x = x.reshape(-1, 1)
     y = y.reshape(-1, 1)
     z = z.reshape(N*N,1)
-    data = SetUpDesignMat(x,y,N)
+    data = SetUpDesignMat(x,y,N,degree)
     x_n, y_n = SetUpGrid(N)
     x_n = x_n.reshape(-1, 1)
     y_n = y_n.reshape(-1, 1)
-    data_n = SetUpDesignMat(x_n,y_n,N)
+    data_n = SetUpDesignMat(x_n,y_n,N,degree)
     return z, data, data_n, x_n, y_n
 
-
-def ConfidentIntervalBeta(Experiments, N):
-    betaBundle = np.zeros(shape=(6,Experiments))
+def ConfidentIntervalBeta(Experiments, N, degree):
+    betaBundle = np.zeros(shape=(degree+1,Experiments))
     for i in range (0, Experiments):
-        z, data, data_n, x_n, y_n = SetUpData(N)
+        z, data, data_n, x_n, y_n = SetUpData(N,degree)
         beta = NaiveSolverOLS(z, data, data_n, x_n, y_n)
-        for j in range (0,6):
+        for j in range (0,degree+1):
             betaBundle[j][i] = beta[j]
-    for i in range (0,6):
+    for i in range (0,degree+1):
         print ('Confindent interval for beta_{0} : [ {1} ; {2} ]' .format(i, Mean(betaBundle[i][:]) - 2*np.std(betaBundle[i][:]),  Mean(betaBundle[i][:]) + 2*np.std(betaBundle[i][:])))
 
 
-
-
 #Number of grid points in one dim
-Experiments = 10
 N = 500
+
+# Poly degree
+degree = 4
+
+# Interval
+Experiments = 10
+
 #Define interval for the arguments of the Franke function
 x_exact = np.arange(0, 1, 0.05)
 y_exact = np.arange(0, 1, 0.05)
-#Round round baby round round 
 
-z, data, data_n, x_n, y_n = SetUpData(N)
+z, data, data_n, x_n, y_n = SetUpData(N,degree)
 print("########################################")
 print("Scikit OSL: ")
-ScikitSolverOLS(data, z)
+ScikitSolverOLS(data, z, degree)
 print(' ============== Scikit Lasso ============')
 ScikitSolverLasso(data, z)
 print(' ============== Scikit Ridge ============')
 ScikitSolverRidge(data, z)
 print(' ============== Naive Ridge ============') 
-NaiveSolverRidge(data, z, x_n, y_n, data_n)
-print("Naive OSL : ")
+NaiveSolverRidge(data, z, x_n, y_n, data_n, degree)
+print(" ============== Naive OSL ============= ")
 NaiveSolverOLS(z, data, data_n, x_n, y_n)
 print(' =============== Naive OLS ConfidentIntervalBeta ==============')  
-ConfidentIntervalBeta(Experiments, N)
-
-
+ConfidentIntervalBeta(Experiments, N, degree)
